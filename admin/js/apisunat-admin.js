da43d82a62fc9b1c2d2776f15304bf6a17dec592
Heikel Villar <heikelvillar@gmail.com>
2022-07-20 01:36:16 -0400

    phpcs

--- admin/js/apisunat-admin.js
+++ admin/js/apisunat-admin.js
@@ -1,69 +1,88 @@
 (function ($) {
-    'use strict';
+	'use strict';
 
-    jQuery(document).ready(function ($) {
-        const apisunat_modal = document.getElementById('apisunatModal');
-        const apisunat_span = document.getElementById('apisunatModalClose');
-        const apisunat_reason = document.getElementById('apisunat_reason');
+	jQuery( document ).ready(
+		function ($) {
+			const apisunat_modal  = document.getElementById( 'apisunatModal' );
 
-        const button_save = document.getElementsByName("save")
+			const button_save = document.getElementsByName( "save" )
 
-        $(document).ready(function () {
-            $("#post").submit(function () {
-                button_save[0].setAttribute('disabled', 'disabled');
-                return true;
-            });
-        });
+			$( document ).ready(
+				function () {
+					$( "#post" ).submit(
+						function () {
+							button_save[0].setAttribute( 'disabled', 'disabled' );
+							return true;
+						}
+					);
+				}
+			);
 
-        $(document).on("click", "#apisunatModalClose", function (e) {
-            apisunat_modal.style.display = "none";
-        });
+			$( document ).on(
+				"click",
+				"#apisunatModalClose",
+				function (e) {
+					apisunat_modal.style.display = "none";
+				}
+			);
 
-        $(document).on("click", "#apisunatSendData", function (e) {
-            e.stopImmediatePropagation();
-            e.preventDefault();
+			$( document ).on(
+				"click",
+				"#apisunatSendData",
+				function (e) {
+					e.stopImmediatePropagation();
+					e.preventDefault();
 
-            let orderId = $('#orderId').val();
-            let orderStatus = $('#orderStatus').val();
+					let orderId     = $( '#orderId' ).val();
+					let orderStatus = $( '#orderStatus' ).val();
 
-            if (orderStatus !== 'completed') {
-                alert("La orden debe completarse para poder enviar los datos");
+					if (orderStatus !== 'completed') {
+						alert( "La orden debe completarse para poder enviar los datos" );
 
-            } else {
+					} else {
 
-                $('#apisunatSendData').hide();
-                $('#apisunatLoading').show();
-                let data = {
-                    action: 'send_apisunat_order',
-                    order_value: orderId
-                };
+						$( '#apisunatSendData' ).hide();
+						$( '#apisunatLoading' ).show();
+						let data = {
+							action: 'send_apisunat_order',
+							order_value: orderId
+						};
 
-                jQuery.post(apisunat_ajax_object.ajax_url, data, async function (response) {
-                    window.location = document.location.href;
-                    // $('#apisunatLoading').hide();
-                });
-            }
-        });
+						jQuery.post(
+							apisunat_ajax_object.ajax_url,
+							data,
+							async function (response) {
+								window.location = document.location.href;
+								// $('#apisunatLoading').hide();
+							}
+						);
+					}
+				}
+			);
 
+			// show/hide for advanced options.
+			if ($( "#apisunat_custom_checkout" ).val() === "false") {
+				$( '.regular-text.regular-text-advanced' ).hide();
+				$( '.regular-text.regular-text-advanced' ).attr( 'required', null )
+			}
 
-        // show/hide for advanced options
-        if ($( "#apisunat_custom_checkout" ).val() === "false") {
-            $('.regular-text.regular-text-advanced').hide();
-            $('.regular-text.regular-text-advanced').attr('required', null)
-        }
+			$( document ).on(
+				"change",
+				"#apisunat_custom_checkout",
+				function(e) {
+					const value = e.target.value;
 
-        $(document).on("change", "#apisunat_custom_checkout", function(e) {
-            const value = e.target.value;
-            
-            if (value === "true") {
-                $('.regular-text.regular-text-advanced').show();
-                $('.regular-text.regular-text-advanced').attr('required', true)
-            }
-            if (value === "false") {
-                $('.regular-text.regular-text-advanced').hide();
-                $('.regular-text.regular-text-advanced').attr('required', null)
-            }
-        });
-    });
+					if (value === "true") {
+						$( '.regular-text.regular-text-advanced' ).show();
+						$( '.regular-text.regular-text-advanced' ).attr( 'required', true )
+					}
+					if (value === "false") {
+						$( '.regular-text.regular-text-advanced' ).hide();
+						$( '.regular-text.regular-text-advanced' ).attr( 'required', null )
+					}
+				}
+			);
+		}
+	);
 
-})(jQuery);
+})( jQuery );
